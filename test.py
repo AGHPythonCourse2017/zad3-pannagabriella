7f4cce5f953460b6424ab5f2b2b85ce5cb194d8c
pannagabriella <gzmkowalik@gmail.com>
2017-06-06 12:05:26 +0200

    update tests

--- test.py
+++ test.py
@@ -1,24 +1,16 @@
-import unittest
 import zad3pannagabriella as gaba
 
-
-class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(gaba.Resolver("Kristen", "Bell", "Kraina lodu").resolve(), gaba.ExitStatus.FOUND)
-        self.assertEqual(gaba.Resolver("Johnny", "Depp", "Charlie").resolve(), gaba.ExitStatus.FOUND)
-        self.assertEqual(gaba.Resolver("Jola", "Rutowicz", "Charlie").resolve(), gaba.ExitStatus.NOT_FOUND)
-        self.assertEqual(gaba.Resolver("Anna", "Wesołowska", "Sędzia").resolve(), gaba.ExitStatus.FOUND)
-        self.assertEqual(gaba.Resolver("Sacha", "Cohen", "Dyktator").resolve(), gaba.ExitStatus.FOUND)
-        self.assertEqual(gaba.Resolver("Charlie", "Chaplin", "Dyktator").resolve(),
-                         gaba.ExitStatus.FOUND_ACTOR_WITH_THAT_LAST_NAME)
-        self.assertEqual(gaba.Resolver("Abelard", "Giza", "Wożonko").resolve(), gaba.ExitStatus.FOUND)
-        self.assertEqual(gaba.Resolver("Jarosław", "Kaczyński", "O dwóch takich co ukradli księżyc").resolve(),
-                         gaba.ExitStatus.FOUND)
-        self.assertEqual(gaba.Resolver("Sylvester", "Stallone", "Ranczo").resolve(), gaba.ExitStatus.NOT_FOUND)
-        self.assertEqual(gaba.Resolver("Sylvester", "Stallone", "Rambo").resolve(), gaba.ExitStatus.FOUND)
-        self.assertEqual(gaba.Resolver("Sylvestro", "Stallone", "Rambo").resolve(),
-                         gaba.ExitStatus.FOUND_ACTOR_WITH_THAT_LAST_NAME)
-
-
-if __name__ == '__main__':
-    unittest.main()
+assert not gaba.Resolver("Kristen", "Bell", "Kraina lodu").resolve() == gaba.ExitStatus.NOT_FOUND
+assert gaba.Resolver("Johnny", "Depp", "Charlie").resolve() == gaba.ExitStatus.FOUND
+assert gaba.Resolver("Jola", "Rutowicz", "Charlie").resolve() == gaba.ExitStatus.NOT_FOUND
+assert not gaba.Resolver("Anna", "Wesołowska", "Sędzia").resolve() \
+           == gaba.ExitStatus.FOUND_ACTOR_WITH_THAT_LAST_NAME
+assert gaba.Resolver("Sacha", "Cohen", "Dyktator").resolve() == gaba.ExitStatus.FOUND
+assert gaba.Resolver("Charlie", "Chaplin", "Dyktator").resolve() \
+       == gaba.ExitStatus.FOUND_ACTOR_WITH_THAT_LAST_NAME
+assert gaba.Resolver("Abelard", "Giza", "Wożonko").resolve() == gaba.ExitStatus.FOUND
+assert gaba.Resolver("Jarosław", "Kaczyński", "O dwóch takich co ukradli księżyc").resolve() == gaba.ExitStatus.FOUND
+assert gaba.Resolver("Sylvester", "Stallone", "Ranczo").resolve() == gaba.ExitStatus.NOT_FOUND
+assert gaba.Resolver("Sylvester", "Stallone", "Rambo").resolve() == gaba.ExitStatus.FOUND
+assert gaba.Resolver("Sylvestro", "Stallone", "Rambo").resolve() \
+       == gaba.ExitStatus.FOUND_ACTOR_WITH_THAT_LAST_NAME
